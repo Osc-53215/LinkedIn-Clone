d41986ce0ddd7790be15fa1feba031a5b716f2e0
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-14 20:38:07 -0500

    created Header component

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
       <h1>LinkedIn Clone!!</h1>
+
+      <Header />
+
+      {/* <App_Body />
+        <Sidebar />
+        <Feed />
+        <Widgets /> */}
+
+
     </div>
   );
 }
