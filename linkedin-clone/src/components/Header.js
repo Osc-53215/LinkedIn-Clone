5b311652b7e7049e0fe3dae120db0cbdd2eb0f79
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-14 22:02:11 -0500

    ran into problem with react hooks inding a solution to continue coding

--- linkedin-clone/src/components/Header.js
+++ linkedin-clone/src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
+    require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
+
+
     return (
+        
         <div className = 'header'>
             <div className = 'header__left'>
                 <img src = '' alt = ''/>
 
                 <div className = 'header__search'>
+                    <SearchIcon />
                     <input type = 'text'/>
                 </div>
+
+                
             </div>
 
             <div className = 'header__right'>
