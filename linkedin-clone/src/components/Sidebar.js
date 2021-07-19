9e81b7a470a3c3a8747f3e28c080c97722d20364
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-19 00:06:33 -0500

    added more styling to sidebar top component

--- linkedin-clone/src/components/Sidebar.js
+++ linkedin-clone/src/components/Sidebar.js
@@ -10,9 +10,9 @@ function Sidebar() {
                 src = 'https://images.unsplash.com/photo-1618335829737-2228915674e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' 
                 alt = ''
                 />
-                <Avatar className = 'sidecar__avatar'/>
+                <Avatar className = 'sidebar__avatar'/>
                 <h2>Oscar Colon</h2>
-                <h2>oscarcolon732@gmail.com</h2>
+                <h4>oscarcolon732@gmail.com</h4>
             </div>
 
             <div className = 'sidebar__stats'>
