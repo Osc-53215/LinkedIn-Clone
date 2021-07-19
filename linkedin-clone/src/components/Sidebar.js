ca8e76a059cdb87bc53bb14d386c4434e7454de5
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-18 22:41:04 -0500

    added avatar component from material ui and added dummy data to get started cosing the sidebar

--- linkedin-clone/src/components/Sidebar.js
+++ linkedin-clone/src/components/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Sidebar.css';
+import { Avatar } from '@material-ui/core';
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className = 'sidebar'>
+            <div className = 'sidebar__top'>
+                <img src = '' alt = ''/>
+                <Avatar />
+                <h2>Oscar Colon</h2>
+                <h2>oscarcolon732@gmail.com</h2>
+            </div>
         </div>
     )
 }
