b5ed5b493ce170382108c035180b167923034be1
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-18 22:48:59 -0500

    added rest of dummy data now going to style it!

--- linkedin-clone/src/components/Sidebar.js
+++ linkedin-clone/src/components/Sidebar.js
@@ -7,9 +7,24 @@ function Sidebar() {
         <div className = 'sidebar'>
             <div className = 'sidebar__top'>
                 <img src = '' alt = ''/>
-                <Avatar />
+                <Avatar className = 'sidecar__avatar'/>
                 <h2>Oscar Colon</h2>
                 <h2>oscarcolon732@gmail.com</h2>
+            </div>
+
+            <div className = 'sidebar__stats'>
+                <div className = 'sidebar__stat'>
+                    <p>Who viewed you</p>
+                    <p className = 'sidebar__statNumber'>23,000</p>
+                </div>
+                <div className = 'sidebar__stat'>
+                    <p>Views on post</p>
+                    <p className = 'sidebar__statNumber'>2000</p>
+                </div>
+            </div>
+
+            <div className = 'sidebar__bottom'>
+                <p>Recent</p>
             </div>
         </div>
     )
