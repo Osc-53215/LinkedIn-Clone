7ea8919bcc42e06fbd238c903959c21966084b97
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-07-24 17:01:07 -0500

    setting up login component

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -3,20 +3,25 @@ import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed'
+import Login from './components/Login';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
 
       <Header />
 
-      <div className = 'app__body'>
-        <Sidebar />
-        <Feed />
-
-      </div>
-
-
+      {!user ? <Login /> :
+      
+        (<div className = 'app__body'>
+          <Sidebar />
+          <Feed />
+        </div>
+      
+      )}
     </div>
   );
 }
