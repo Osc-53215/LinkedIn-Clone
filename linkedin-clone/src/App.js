c1551d6cbaa7c18c24335dd701bf9504aba3b000
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-09 12:27:28 -0500

    added linked in login image

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -5,22 +5,23 @@ import Sidebar from './components/Sidebar';
 import Feed from './components/Feed'
 import Login from './components/Login';
 import { selectUser } from './features/userSlice';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const user = useSelector(selectUser);
+  const user = useSelector(selectUser)
 
   return (
     <div className="app">
 
       <Header />
 
-      {!user ? <Login /> :
-      
-        (<div className = 'app__body'>
+      {!user ? ( 
+        <Login />
+        ) : (
+        <div className = 'app__body'>
           <Sidebar />
           <Feed />
         </div>
-      
       )}
     </div>
   );
