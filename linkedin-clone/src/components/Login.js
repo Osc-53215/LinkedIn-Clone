358ce73d8967dc2010a49cff02e7e9c236ebee08
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-09 12:54:57 -0500

    added styling to make webpage look better

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -4,7 +4,12 @@ import './Login.css';
 function Login() {
     return (
         <div className = 'login'>
-            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="" />
+            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" 
+            alt="" />
+
+            <form>
+                <input placeholder = 'Full name (required if registering)' type="text" />
+            </form>
         </div>
     )
 }
