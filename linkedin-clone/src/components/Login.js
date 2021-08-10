d8feca640af694a0a6b1a678c71ffbd2f0726066
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-10 14:15:13 -0500

    setting up state to create functions

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../firebase'
 import './Login.css';
 
 function Login() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    const loginToApp = (e) => {
+        e.preventDefault();
+    };
+
     const register = () => {};
-    const loginToApp = () => {};
 
 
     return (
