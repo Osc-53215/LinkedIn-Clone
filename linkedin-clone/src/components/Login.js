0f02d8f21a1bed3c048a7b65fc3709f5793bcd5b
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-10 14:02:22 -0500

    added styling to form as well as input boxes in the form

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
+
+    const register = () => {};
+    const loginToApp = () => {};
+
+
     return (
         <div className = 'login'>
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" 
@@ -12,8 +17,12 @@ function Login() {
                 <input placeholder = 'Profile pic URL (optional)' type="text" />
                 <input placeholder = 'Email' type="email" />
                 <input placeholder = 'Password' type="password" />
-                <button>Sign In</button>
+                <button type = 'submit' onClick = {loginToApp}>Sign In</button>
             </form>
+
+            <p>Not a member?
+                <span className = 'login__register' onClick = {register}>Register Now</span>
+            </p>
         </div>
     )
 }
