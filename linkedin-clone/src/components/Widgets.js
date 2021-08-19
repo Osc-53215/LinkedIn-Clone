ebc36f33b27814a8a425f25a6f6bccff65455ad5
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-19 11:54:51 -0500

    added some jsx to widgets

--- linkedin-clone/src/components/Widgets.js
+++ linkedin-clone/src/components/Widgets.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './Widgets.css';
 import InfoIcon from '@material-ui/icons/Info';
+import FiberManualRecord from '@material-ui/icons/FiberManualRecord';
 
 function Widgets() {
+
+    const newsArticle = (header, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecord />
+            </div>
+            <div className="widgets__articleRight">
+                <h4>{header}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    )
+    
     return (
         <div className = 'widgets'>
             <div className= "widgets__header">
                 <h2>LinkedIn News</h2>
                 <InfoIcon />
             </div>
+            {newsArticle('This is a test', 'Top news - 999 readers')}
         </div>
     );
 }
